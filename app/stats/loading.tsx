d9f812eb6cb0d441941cb3b1501eb538874f2bd5
Yordan Ganev <yordanganew@gmail.com>
2023-09-20 18:25:37 +0300

    fix loading message

--- app/stats/loading.tsx
+++ app/stats/loading.tsx
@@ -4,7 +4,7 @@ import Style from "./loading.module.css";
 export default function loading() {
   return (
     <>
-      <div>Loading Matches data..</div>
+      <div className={Style.info}>Loading Matches data..</div>
       <div className={Style.wrapper}>
         <Loading message={null} />
       </div>
